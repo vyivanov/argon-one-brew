50588c301b27e30856b3e402e8fa79f67879e4c0
Vladimir Ivanov <inbox@vova-ivanov.info>
2020-05-24 20:06:38 +0300

    added temp_to_pwm function

--- argon-one-salt.cpp
+++ argon-one-salt.cpp
@@ -21,6 +21,22 @@ void signal_handler(const int /*sig*/)
     cv.notify_all();
 }
 
+constexpr uint8_t kTempTrig_10 {50};
+constexpr uint8_t kTempTrig_50 {55};
+constexpr uint8_t kTempTrig_99 {60};
+
+uint8_t temp_to_pwm(const uint8_t temp) noexcept
+{
+    uint8_t pwm = {};
+
+    if      (                        temp < kTempTrig_10) { pwm =  0; }
+    else if (kTempTrig_10 <= temp && temp < kTempTrig_50) { pwm = 10; }
+    else if (kTempTrig_50 <= temp && temp < kTempTrig_99) { pwm = 50; }
+    else if (kTempTrig_99 <= temp                       ) { pwm = 99; }
+
+    return pwm;
+}
+
 }
 
 int main()
@@ -32,14 +48,14 @@ int main()
 
     std::future<int> thr_temp = std::async(std::launch::async,
         [&]() -> int {
-            int32_t temp = 0;
-            auto zone = std::ifstream("/sys/class/thermal/thermal_zone0/temp");
+            int32_t temp_milli = 0;
+            auto zone_file = std::ifstream("/sys/class/thermal/thermal_zone0/temp");
             std::unique_lock lk(mtx);
             while (is_run) {
-                zone >> temp;
-                std::cout << "temp = " << temp / 1000.F << '\n';
+                zone_file >> temp_milli;
+                std::cout << "pwm = " << temp_to_pwm(temp_milli / 1000.F) << '\n';
                 cv.wait_for(lk, 1s, [&]() { return false == is_run; });
-                zone.seekg(0);
+                zone_file.seekg(0);
             }
             return 0;
         }
